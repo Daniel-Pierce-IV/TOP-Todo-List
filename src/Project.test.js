23f060780270a0779fb694cacf5ddcefb5293d6f
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-04-02 12:34:28 -0500

    Add test for Project tasks

--- src/Project.test.js
+++ src/Project.test.js
@@ -5,4 +5,15 @@ describe('Projects', () => {
         const project = new Project('Project Title');
         expect(project.title).toBe('Project Title');
     });
+
+    test('can have tasks', () => {
+        const project = new Project('Project Title', [
+            new Task('Task #1'),
+            new Task('Task #2')
+        ]);
+
+        expect(project.tasks.length).toBe(2);
+        expect(project.tasks[0].title).toBe('Task #1');
+        expect(project.tasks[1].title).toBe('Task #2');
+    });
 });
