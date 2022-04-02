3b22505c013b00678993e4ac224a169371bb9ba9
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-04-01 19:37:25 -0500

    Add test for Priority's properties value type

--- src/Priority.test.js
+++ src/Priority.test.js
@@ -4,4 +4,10 @@ describe('Priority', () => {
     test('has 2 types', () => {
         expect(Object.getOwnPropertyNames(Priority).length).toBe(2);
     });
+
+    test('type property values are of type symbol', () => {
+        for (const key in Priority) {
+            expect(typeof Priority[key]).toBe('symbol');
+        }
+    });
 });
