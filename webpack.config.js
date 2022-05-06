45ad4180571730428b0e1d5bf40fb38cb9a243ce
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-05-06 14:07:25 -0500

    Create Prettier config file, format webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devServer: {
-        static: './dist',
-    },
-    entry: './src/index.js',
-    output: {
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
+  entry: './src/index.js',
+  output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Todo List',
-        scriptLoading: 'defer'
-    })],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo List',
+      scriptLoading: 'defer',
+    }),
+  ],
 };
