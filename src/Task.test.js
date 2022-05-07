dfc819c1957c166f8aa5c7952eae9086d8324c30
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-05-07 17:46:06 -0500

    Covert double quotes to single

--- src/Task.test.js
+++ src/Task.test.js
@@ -1,34 +1,34 @@
-import Task from "./Task";
-import Priority from "./Priority";
+import Task from './Task';
+import Priority from './Priority';
 
-describe("Tasks", () => {
-  test("have a title", () => {
-    const task = new Task("Some Title");
-    expect(task.title).toBe("Some Title");
+describe('Tasks', () => {
+  test('have a title', () => {
+    const task = new Task('Some Title');
+    expect(task.title).toBe('Some Title');
   });
 
-  test("can have a description", () => {
-    const task = new Task("Some Title", { description: "A neat description" });
-    expect(task.description).toBe("A neat description");
+  test('can have a description', () => {
+    const task = new Task('Some Title', { description: 'A neat description' });
+    expect(task.description).toBe('A neat description');
   });
 
-  test("can have a deadline", () => {
-    const task = new Task("Some Title", { deadline: "2022-04-01" });
-    expect(task.deadline).toBe("2022-04-01");
+  test('can have a deadline', () => {
+    const task = new Task('Some Title', { deadline: '2022-04-01' });
+    expect(task.deadline).toBe('2022-04-01');
   });
 
-  test("can have a priority", () => {
-    const task = new Task("Some Title", { priority: Priority.HIGH });
+  test('can have a priority', () => {
+    const task = new Task('Some Title', { priority: Priority.HIGH });
     expect(task.priority).toBe(Priority.HIGH);
   });
 
-  test("have a done status that defaults to false", () => {
-    const task = new Task("Some Title");
+  test('have a done status that defaults to false', () => {
+    const task = new Task('Some Title');
     expect(task.isDone).toBe(false);
   });
 
-  test("can be completed", () => {
-    const task = new Task("Some Title");
+  test('can be completed', () => {
+    const task = new Task('Some Title');
     task.complete();
 
     expect(task.isDone).toBe(true);
