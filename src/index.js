2b0068977007a6c0e05536a071c07e98be36cc53
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-05-06 21:57:31 -0500

    Create Project UI dynamically

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Project from './Project';
 
+const projectsElement = document.querySelector('#projects');
+const projectTitleElement = document.querySelector('#content header h2');
+
 const projectClasses = [
   'project',
   'cursor-pointer',
@@ -8,16 +11,23 @@ const projectClasses = [
   'active:bg-gray-300',
 ];
 
-// TODO import existing projects. If empty, create default project
-
-const projects = [new Project('Default Project')];
-
-const projectsElement = document.querySelector('#projects');
 const baseProjectElement = document.createElement('li');
 baseProjectElement.classList.add(...projectClasses);
 
 const newProjectButton = document.querySelector('#new-project');
 const newProjectDialog = document.querySelector('#new-project-dialog');
+
+// TODO import existing projects. If empty, create default project
+
+const projects = [];
+let currentProject;
+
+if (!projects.length) {
+  createProject({ title: 'Default Project' });
+}
+
+// Latest-created project is the default on page load
+currentProject = projects[projects.length - 1];
 
 newProjectButton.addEventListener('click', () => {
   // Always present user with an empty form
@@ -40,26 +50,34 @@ newProjectDialog.addEventListener('close', () => {
 function createProject(data) {
   const project = new Project(data.title);
   projects.push(project);
-
-  // Update DOM
-  removeActiveClass(projectsElement.querySelectorAll('.project'));
-  const projectElement = createProjectElement(project);
-  projectsElement.prepend(projectElement);
+  project.element = createProjectElement(project);
+  projectsElement.prepend(project.element);
+  changeProject(project);
 }
 
 function createProjectElement(project) {
-  const projectElement = baseProjectElement.cloneNode();
-  projectElement.textContent = project.title;
-  projectElement.classList.add('active');
-  return projectElement;
+  const element = baseProjectElement.cloneNode();
+  element.textContent = project.title;
+  element.classList.add('active');
+  element.addEventListener('click', changeProject.bind(null, project));
+
+  return element;
 }
 
-function removeActiveClass(elements) {
-  if (elements.forEach) {
-    projectsElement.querySelectorAll('.project').forEach((projectElement) => {
-      projectElement.classList.remove('active');
-    });
-  } else {
-    elements.classList.remove('active');
+function changeProject(project) {
+  updateCurrentProject(project);
+  updateProjectSection();
+}
+
+function updateCurrentProject(project) {
+  if (currentProject) {
+    currentProject.element.classList.remove('active');
   }
+
+  project.element.classList.add('active');
+  currentProject = project;
+}
+
+function updateProjectSection() {
+  projectTitleElement.textContent = currentProject.title;
 }
